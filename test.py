656d02e9d2bb8af2ae24fdd992fe6e057c614d25
Kenta Yamada <knt.yamada.800@gamil.com>
2019-12-22 21:28:13 +0900

    #2 Replace code

--- test.py
+++ test.py
@@ -1,102 +1,132 @@
-# -*- coding: utf-8 -*-
 import unittest
-import psycopg2
 from pgadapter import PgAdapter
 
 
-TEST_CASES = {
-    'error_message': 'Failed {0}, no. {1}. except: {2}, result: {3}',
-    'test_save_ok': [
-        {'no': 1, 'expect': True, 'data': ('test', 100)}
-    ],
-    'test_save_invalid_value': [
-        {'no': 1, 'data': (None, 100)},
-        {'no': 2, 'data': ('test', None)},
-        {'no': 3, 'data': (None, None)},
-        {'no': 4, 'data': (100, 'test')}
-    ]
-}
-
-SETUP_DATA = [
-    {'name': 'Taro', 'age': 26},
-    {'name': 'Taro', 'age': 24},
-    {'name': 'Hanako', 'age': 24},
-    {'name': 'Tom', 'age': 22}
-]
-
-DSN = {
-    'host': '192.168.56.101',
-    'dbname': 'pgadapter_db',
-    'user': 'kenta',
-    'password': 'kenta'
-}
+class CarMaker:
+    def __init__(self, id=None, name='', **kwargs):
+        self.id = id
+        self.name = name
 
 
 class TestPgAdapter(unittest.TestCase):
-    def setUp(self):
-        self.db = PgAdapter(DSN)
-        for d in SETUP_DATA:
-            args = (d['name'], d['age'])
-            self.db.save('save_person', args)
-            self.db.commit()
+    @classmethod
+    def setUpClass(cls):
+        cls.dsn = {
+            'host': 'localhost',
+            'dbname': 'pg_adapter',
+            'user': 'root',
+            'password': 'root',
+            'port': 5432
+        }
 
     def tearDown(self):
-        self.db = PgAdapter(DSN, commit=True)
-        self.db.query('DELETE FROM  persons;')
+        self.__cleanup()
 
-    def test_save_ok(self):
-        data = TEST_CASES['test_save_ok'][0]
-        saved = self.db.save('save_person', data['data'])
-        self.db.commit()
-        self.assertEqual(saved, data['expect'])
+    def test_connect_success(self):
+        db = PgAdapter(self.dsn)
+        self.assertTrue(db.connected)
 
-    def test_save_invalid_value(self):
-        data = TEST_CASES['test_save_invalid_value']
-        with self.assertRaises(psycopg2.IntegrityError):
-            for d in data:
-                self.db.save('save_person', d['data'])
-        self.db.rollback()
+    def test_connect_with_context(self):
+        with PgAdapter(self.dsn) as db:
+            self.assertTrue(db.connected)
 
-    def test_delete_ok(self):
-        deleted = self.db.delete('delete_person', ('test',))
-        self.db.commit()
-        self.assertEqual(deleted, True)
+    def test_connect_failure_when_dsn_is_null(self):
+        with self.assertRaises(Exception):
+            db = PgAdapter(None)
 
-    def test_delete_rowcount_zero(self):
-        deleted = self.db.delete('delete_person', ('Who are you',))
-        self.db.commit()
-        self.assertEqual(deleted, True)
-
-    def test_find_ok(self):
-        self.db.auto_commit = True
-        rows = self.db.find('find_persons', ('Taro',))
-        self.assertEqual(2, len(rows))
-
-    def test_find_one_ok(self):
-        self.db.auto_commit = True
-        row = self.db.find_one('find_persons', ('Hanako',))
-        self.assertEqual('Hanako', row['name'])
-
-    def test_invalid_connection(self):
-        dsn = DSN.copy()
-        dsn['host'] = 'invalid host'
-        self.db = PgAdapter(dsn)
-        with self.assertRaises(psycopg2.OperationalError):
-            self.db.query('SELECT * FROM persons;')
-            self.db.save('save_person', ('test', 100))
-            self.db.delete('save_person', ('test',))
-        self.db.rollback()
-
-    def test_invalid_dsn(self):
+    def test_run_commands_when_command_is_empty(self):
+        db = PgAdapter(self.dsn)
         with self.assertRaises(ValueError):
-            self.db.switch_db(None)
-        with self.assertRaises(KeyError):
-            self.db.switch_db({})
+            db.execute(None)
+            db.execute('')
+            db.execute_many(None)
+            db.execute_many('')
+            db.execute_proc(None)
+            db.execute_proc('')
+            db.fetch(None)
+            db.fetch('')
+            db.fetch_one(None)
+            db.fetch_one('')
+            db.fetch_proc(None)
+            db.fetch_proc('')
+            db.fetch_one_proc(None)
+            db.fetch_one_proc('')
 
-    def test_load_invalid_filepath(self):
-        filepath = 'notfound'
-        with self.assertRaises(FileNotFoundError):
-            PgAdapter.load_sqlfile(filepath)
+    def test_run_commands_when_command_is_invalid_type(self):
+        db = PgAdapter(self.dsn)
+        with self.assertRaises(TypeError):
+            db.execute(1)
+            db.execute_many(1)
+            db.execute_proc(1)
+            db.fetch(1)
+            db.fetch_one(1)
+            db.fetch_proc(1)
+            db.fetch_one_proc(1)
+
+    def test_execute_success(self):
+        query = 'INSERT INTO car_makers (name) VALUES (%s);'
+        affected = 0
+        with PgAdapter(self.dsn) as tran:
+            affected = tran.execute(query, ('toyota',))
+        self.assertEqual(affected, 1)
+
+    def test_fetch_success(self):
+        self.__setup_car_makers()
+        rows = None
+        with PgAdapter(self.dsn) as tran:
+            rows = tran.fetch('SELECT * FROM car_makers;')
+        self.assertEqual(3, len(rows))
+
+    def test_fetch_one(self):
+        self.__setup_car_makers()
+        row = None
+        with PgAdapter(self.dsn) as tran:
+            query = 'SELECT * FROM car_makers WHERE name like %s;'
+            row = tran.fetch_one(query, ('%nis%',))
+        self.assertIsNotNone(row)
+        self.assertEqual(row['name'], 'nissan')
+
+    def test_execute_proc(self):
+        affected = 0
+        with PgAdapter(self.dsn) as tran:
+            affected = tran.execute_proc('save_car_maker', (None, 'toyota',))
+        self.assertEqual(affected, 1)
+
+    def test_fetch_proc(self):
+        self.__setup_car_makers()
+        rows = None
+        with PgAdapter(self.dsn) as tran:
+            rows = tran.fetch_proc('find_car_makers')
+        self.assertEqual(3, len(rows))
+
+    def test_fetch_one_proc(self):
+        self.__setup_car_makers()
+        row = None
+        with PgAdapter(self.dsn) as tran:
+            row = tran.fetch_one_proc('find_car_maker_by_id', (1,))
+        self.assertIsNotNone(row)
+
+    def test_fetch_one_proc_when_mapping_object(self):
+        self.__setup_car_makers()
+        car_maker = None
+        with PgAdapter(self.dsn) as tran:
+            row = tran.fetch_one_proc('find_car_maker_by_id', (1,))
+            car_maker = CarMaker(**row)
+        self.assertIsNotNone(car_maker)
+        self.assertEqual('toyota', car_maker.name)
+
+    def __setup_car_makers(self):
+        query = 'INSERT INTO car_makers (name) VALUES (%s);'
+        makers = [('toyota',), ('nissan',), ('honda',)]
+        with PgAdapter(self.dsn) as tran:
+            tran.execute_many(query, makers)
+
+    def __cleanup(self):
+        with PgAdapter(self.dsn) as tran:
+            tables = ['car_makers', 'cars']
+            for table in tables:
+                query = 'TRUNCATE TABLE {} RESTART IDENTITY;'.format(table)
+                tran.execute(query)
 
 
 if __name__ == '__main__':
